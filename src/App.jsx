e9e88c373e455c4896cb7f00f9f370afba0dd72f
Khizarazam <khizarazam143@gmail.com>
2024-03-29 18:54:06 +0500

    what ever you type.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import { Outlet } from 'react-router-dom';
+import Header from './Components/Header'
+import Footer from  './Components/Footer';
 
 function App() {
 
   return (
     <>
-     
+     <Header />
+     <Outlet />
+     <Footer />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './Components/Home.jsx'
+import App from './App.jsx'
+import About  from './Components/About.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "about",
+        element: <About/>
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
